1fe576a343018e67d9af345832285a08631e58e8
Wesy32 <wesy32@gmail.com>
2022-02-28 07:10:15 -0800

    Player score tracking logic implemented.

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ function winRound(winner, loser) {
 function loseRound(winner, loser) {
     return "You lose! " + winner + " beats " + loser;
 }
+
 function playRound(playerSelection, computerSelection) {
     let upperP = playerSelection.toUpperCase();
     let upperC = computerSelection.toUpperCase();
@@ -40,9 +41,11 @@ function playRound(playerSelection, computerSelection) {
 
 
 const buttons = document.querySelectorAll('button');
-let playerSelection = "ERROR: You shoudln't see this...";
+let playerSelection = "ERROR: You shouldn't see this...";
 let computerSelection = computerPlay();
 let count = 0;
+let playerScore = 0;
+let compScore = 0;
 let resultText = "";
 
 buttons.forEach((button) => {
@@ -55,7 +58,19 @@ buttons.forEach((button) => {
         resultText = playRound(playerSelection, computerSelection);
 
         //TODO: Logic for score tracking, game rounds, and winning/losing.
-        //TODO: change print out  result to 1, not "win.." "win.." "lose..."
+        const resultArray = resultText.split(' ');
+        resultArray.forEach((element) => {
+            if(element === "win!") {
+                playerScore++;
+            }
+            else if(element === "lose!") {
+                compScore++;
+            }
+            else{
+            }
+        });
+        console.log(resultArray);
+        //TODO: change print out result to 1, not "win.." "win.." "lose..."
 
 
 
@@ -63,7 +78,6 @@ buttons.forEach((button) => {
 
         //Creating DOM elements
         const resultContainer = document.querySelector('.resultContainer'); 
-
         const flexContainer = document.createElement('div');
         flexContainer.classList.add('flexContainer');
         //Prints results
@@ -71,16 +85,16 @@ buttons.forEach((button) => {
         content.classList.add('content');
         content.textContent = resultText;
         //Printing scores
-        const playerScore = document.createElement('div');
-        playerScore.classList.add('scoreNum');
-        playerScore.textContent = "Buttholes.";
-        const compScore = document.createElement('div');
-        compScore.classList.add('scoreNum');
-        compScore.textContent = "not butts";
-
-        flexContainer.appendChild(playerScore);
+        const playerDisplay = document.createElement('div');
+        playerDisplay.classList.add('scoreNum');
+        playerDisplay.textContent = playerScore;
+        const compDisplay = document.createElement('div');
+        compDisplay.classList.add('scoreNum');
+        compDisplay.textContent = compScore;
+        //Appending
+        flexContainer.appendChild(playerDisplay);
         flexContainer.appendChild(content);
-        flexContainer.appendChild(compScore);
+        flexContainer.appendChild(compDisplay);
         resultContainer.appendChild(flexContainer);
 
         //if()
